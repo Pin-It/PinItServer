af267e017c2d7ce57a02431487a3087464cf3019
Noel Lee <kyl116@ic.ac.uk>
2018-05-29 17:55:16 +0100

    Added Pin model

--- ping/api/models.py
+++ ping/api/models.py
@@ -1,3 +1,22 @@
+from enum import Enum
+
 from django.db import models
 
-# Create your models here.
+
+class Pin(models.Model):
+
+    class Type(Enum):
+        PICKPOCKET = "Pickpocket"
+        DRUNK = "Drunk"
+        ROBBERY = "Robbery"
+        SCAM = "Scam"
+        HARASSMENT = "Harassment"
+        OTHERS = "Others"
+
+        @classmethod
+        def choices(cls):
+            return [(t.name, t.value) for t in cls]
+
+    pin_type = models.CharField(max_length=10, choices=Type.choices())
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
