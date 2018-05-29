fa4c63f26167b9d86cd9dbbf4a8578e3d54b3696
Noel Lee <kyl116@ic.ac.uk>
2018-05-29 21:29:04 +0100

    Added Comment model

--- ping/api/models.py
+++ ping/api/models.py
@@ -20,3 +20,8 @@ class Pin(models.Model):
     pin_type = models.CharField(max_length=10, choices=Type.choices())
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+class Comment(models.Model):
+    pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
+    text = models.TextField()
