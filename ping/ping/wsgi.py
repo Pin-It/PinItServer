d7090293b5f6318442bc816762ba8dd70a2cb877
Noel Lee <kyl116@ic.ac.uk>
2018-05-29 17:17:11 +0100

    Renamed app to ping

--- ping/ping/wsgi.py
+++ ping/ping/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for travalert project.
+WSGI config for ping project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travalert.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ping.settings")
 
 application = get_wsgi_application()
