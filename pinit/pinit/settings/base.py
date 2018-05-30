89bbb12e52df77b98f5c85ac484162ea46b77e29
Noel Lee <kyl116@ic.ac.uk>
2018-05-30 03:56:46 +0100

    Added Django REST Framework permission settings

--- pinit/pinit/settings/base.py
+++ pinit/pinit/settings/base.py
@@ -116,3 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django REST Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
