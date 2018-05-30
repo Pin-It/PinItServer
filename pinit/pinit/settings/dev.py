a6a896694fd526a8cc30cbf3e469e72f659af60c
Noel Lee <kyl116@ic.ac.uk>
2018-05-30 19:20:52 +0100

    Allow all hosts in dev settings

--- pinit/pinit/settings/dev.py
+++ pinit/pinit/settings/dev.py
@@ -8,6 +8,8 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qd1#yo1&-nhw8rw$sh2@xa5(p8i(l^e!4p)7emcqmtiuw2+j+o'
 
+ALLOWED_HOSTS = ['*']
+
 # Sensitive settings e.g. passwords should be stored in local.py
 try:
     from .local import *
