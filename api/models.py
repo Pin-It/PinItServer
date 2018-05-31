929b82c4c60ca58decfd3f7d01c9c57a525961c3
Noel Lee <kyl116@ic.ac.uk>
2018-05-31 04:16:20 +0100

    Changed pin_type field type

--- api/models.py
+++ api/models.py
@@ -4,26 +4,29 @@ from django.db import models
 
 
 class Pin(models.Model):
+    PICKPOCKET = 1
+    DRUNK = 2
+    ROBBERY = 3
+    SCAM = 4
+    HARASSMENT = 5
+    OTHERS = 6
 
-    class Type(Enum):
-        PICKPOCKET = "Pickpocket"
-        DRUNK = "Drunk"
-        ROBBERY = "Robbery"
-        SCAM = "Scam"
-        HARASSMENT = "Harassment"
-        OTHERS = "Others"
+    TYPE_CHOICES = (
+        (PICKPOCKET, "Pickpocket"),
+        (DRUNK, "Drunk"),
+        (ROBBERY, "Robbery"),
+        (SCAM, "Scam"),
+        (HARASSMENT, "Harassment"),
+        (OTHERS, "Others"),
+    )
 
-        @classmethod
-        def choices(cls):
-            return [(t.name, t.value) for t in cls]
-
-    pin_type = models.CharField(max_length=10, choices=Type.choices())
+    pin_type = models.IntegerField(choices=TYPE_CHOICES)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return '{}@({}, {})'.format(
-            self.pin_type, self.latitude, self.longitude)
+            self.get_pin_type_display(), self.latitude, self.longitude)
 
 
 class Comment(models.Model):
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Pin
+
+
+class PinTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Pin.objects.create(
+            pin_type=Pin.SCAM,
+            latitude=51.508531,
+            longitude=-0.076132
+        )
+
+    def test_pin_str_representation(self):
+        pin = Pin.objects.get(id=1)
+        self.assertTrue(isinstance(pin, Pin))
+        self.assertEqual(str(pin), 'Scam@(51.508531, -0.076132)')
