10c57ecc38eb7767ee2eb373239b086f6da69b86
Noel Lee <leekayau@gmail.com>
2018-06-01 06:59:14 +0100

    Added post_save handlers to user profile

--- api/models.py
+++ api/models.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -9,6 +11,17 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
 class Pin(models.Model):
