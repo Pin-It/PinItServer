853d5c2fe80b521a7ad53678ef4f8f5840c239c0
Noel Lee <leekayau@gmail.com>
2018-06-01 04:13:11 +0100

    Disabled rest framework permissions

--- pinit/settings.py
+++ pinit/settings.py
@@ -126,11 +126,11 @@ STATIC_URL = '/static/'
 
 # Django REST Framework settings
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
 
 
 # Heroku settings
