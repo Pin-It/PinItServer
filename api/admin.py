143664c7ccdf0370bcec7411037eb01a6237d44e
Noel Lee <leekayau@gmail.com>
2018-06-01 06:02:10 +0100

    Added UserProfile model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Comment, Pin
+from .models import Comment, Pin, UserProfile
 
 admin.site.register(Pin)
 admin.site.register(Comment)
+admin.site.register(UserProfile)
--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
 from enum import Enum
 
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Pin(models.Model):
@@ -23,6 +31,7 @@ class Pin(models.Model):
     pin_type = models.IntegerField(choices=TYPE_CHOICES)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    by_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{}@({}, {})'.format(
@@ -32,3 +41,4 @@ class Pin(models.Model):
 class Comment(models.Model):
     pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
     text = models.TextField()
+    by_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
