b582acba8dcd32d7c8993966dacf12e5f946c70b
Noel Lee <leekayau@gmail.com>
2018-06-01 07:41:55 +0100

    Fixed tests

--- api/tests.py
+++ api/tests.py
@@ -8,12 +8,12 @@ class PinTest(TestCase):
     def setUpTestData(cls):
         Pin.objects.create(
             pin_type=Pin.SCAM,
-            latitude=51.508531285930100,
-            longitude=-0.076132304958301
+            latitude=51.508531200000000,
+            longitude=-0.076132300000000
         )
 
     def test_pin_str_representation(self):
         pin = Pin.objects.get(id=1)
         self.assertTrue(isinstance(pin, Pin))
         self.assertEqual(str(pin),
-                         'Scam@(51.508531285930100, -0.076132304958301)')
+                         'Scam@(51.508531200000000, -0.076132300000000)')
