99d84aad8c0efe8010096adb2ae19321cb6abf11
Noel Lee <leekayau@gmail.com>
2018-06-05 02:14:56 +0100

    Added SessionAuthentication to django rest settings

--- pinit/settings.py
+++ pinit/settings.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
