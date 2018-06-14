fe0d2bcb83d5dac3c41fb78cf3f6bd9b29053f2f
Noel Lee <leekayau@gmail.com>
2018-06-14 17:58:23 +0100

    Made like list only show likes of current user and accept 'pin' parameter

--- api/views.py
+++ api/views.py
@@ -17,3 +17,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+
+    def get_queryset(self):
+        '''
+        Only return the likes created by the current user.
+        Optional `pin` parameter is used to filter the list by associated pin
+        '''
+        user = self.request.user
+        queryset = Like.objects.filter(by_user_id=user.id)
+        pin = self.request.query_params.get('pin', None)
+        if pin is not None:
+            queryset = queryset.filter(pin=pin)
+        return queryset
