ed06e59b085eea2ecd76a41c97b9022e82abb92a
Noel Lee <leekayau@gmail.com>
2018-06-18 01:40:10 +0100

    Added DeviceLocation model

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,8 @@ router.register('device-location', views.DeviceLocationViewSet)
 urlpatterns = [
     path('devices/', views.DeviceViewSet.as_view({'post': 'create'}),
          name='create_fcm_device'),
-    path('device-location/<device__registration_id>/', views.DeviceLocationViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path('device-location/<device__registration_id>/',
+         views.DeviceLocationViewSet.as_view(
+            {'get': 'retrieve', 'patch': 'partial_update'})),
     path('', include(router.urls)),
 ]
